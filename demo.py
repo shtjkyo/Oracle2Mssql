2101bd90087c6b432dbc6e1669fb20ee183c2fa3
shtjkyo <302993359@qq.com>
2018-09-05 14:22:22 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -14,21 +14,19 @@ os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
 oracle_conn = cx_Oracle.connect('admin/admin@localhost:1521/orcl')
 mssql_conn = pymssql.connect(server="admin",port="3723",user="sa",password="admin",database="Cash",charset="utf8")
 #oracle2mssql
-mysql_cur = mysql_conn.cursor()
+oracle_cur = oracle_conn.cursor()
 
-mysql_cur.execute("select Id,FlowDateTime,Mall_Enter,Mall_Exit,UpdateTime from T_Mall_Flow where To_DAYS(UpdateTime)=TO_DAYS(now())")
-resultmysql = mysql_cur.fetchall()
+oracle_cur.execute("select TRANSSTORE,TRADEDATE,TTLORGAMOUNT,POSTTIME from possalestotal t where trunc(TRADEDATE)=trunc(sysdate) ")
+resultoracle = oracle_cur.fetchall()
 mssql_cur = mssql_conn.cursor()
-for row in resultmysql:
-    if row[0]==0:
+for row in resultoracle:
+    if row[0].strip() =='':
        mssql_conn.commit()
        oracle_conn.close()
        mssql_conn.close()
        mysql_conn.close() 
     else:
-        mssql_cur.execute("insert into [Cash].[dbo].[T_Mall_Flow] ( Id,FlowDateTime,Mall_Enter,Mall_Exit,UpdateTime ) values(%s,%s,%s,%s,%s)",(row))
-        #print "%s,%s,%s,%s,%s"%(row)
-    #print row[0].decode('utf8'),row[1].decode('utf8'),row[2].decode('utf8')
+        mssql_cur.execute("insert into [Cash].[dbo].[possalestotal] ( TRANSSTORE,TRADEDATE,TTLORGAMOUNT,POSTTIME ) values(%s,%s,%s,%s)",(row))
 mssql_conn.commit()
 oracle_conn.close()
 mssql_conn.close()
